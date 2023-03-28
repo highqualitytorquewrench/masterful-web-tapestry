3b4e698bd87767fd91f7543de934a0bfd1625fdd
Zach Klagge <klagge.zach@gmail.com>
2023-03-27 20:57:08 -0500

    Hopefully this works

---
+++ contact-form/submit-form.php
@@ -0,0 +1,23 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(strip_tags(trim($_POST["name"])));
+    $email = htmlspecialchars(strip_tags(trim($_POST["email"])));
+    $message = htmlspecialchars(strip_tags(trim($_POST["message"])));
+
+    // Plz no steal mah email thx y'all
+    $to = "klagge@uchicago.edu";
+    $subject = "New message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    mail($to, $subject, $message, $headers);
+
+    // Redirect to a thank you page or back to the contact form
+    header("Location: index.html?submitted=true");
+    exit;
+}
+
+?>
